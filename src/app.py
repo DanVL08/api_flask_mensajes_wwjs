a767dfbe4618e93d378076daf8c11e07dc84b4da
Daniel Vargas <daniellopez882017@gmail.com>
2024-03-07 20:36:29 -0600

    Rutas para leer modificadas, conexion a base de datos prefeco y rutas para , eliminar, agregar, actualizar han sido eliminadas

--- src/app.py
+++ src/app.py
@@ -5,83 +5,35 @@ app = Flask(__name__)
 
 conexion = MySQL(app)
 
-@app.route('/cursos', methods=['GET'])
-def listar_cursos():
+@app.route('/alumnos', methods=['GET'])
+def listar_alumnos():
     try:
         cursor = conexion.connection.cursor()
-        sql = "SELECT codigo, nombre, creditos FROM curso"
+        sql = "SELECT nombre, apellido1, matricula FROM alumnos"
         cursor.execute(sql)
         datos = cursor.fetchall()
-        cursos = []
-        #AGREGAR LOS DATOS DEL CURSO A UN JSON
+        alumnos = []
+        #AGREGAR LOS DATOS DE LOS ALUMNOS A UN JSON
         for fila in datos:
-            curso = {'codigo':fila[0], 'nombre':fila[1], 'creditos': fila[2]}
-            cursos.append(curso)
-        return jsonify({'cursos':cursos, 'mensaje':"Cursos listados!"})
+            alumno = {'nombre':fila[0], 'apellido1':fila[1], 'matricula': fila[2]}
+            alumnos.append(alumno)
+        return jsonify({'alumnos':alumnos, 'mensaje':"Alumnos listados!"})
     except Exception as ex:
         return jsonify({'mensaje':"Error"})
 
- #LEE UN SOLO CURSO Y LO MUESTRA    
-@app.route('/cursos/<codigo>', methods=['GET'])
-def leer_curso(codigo):
+ #LEE UN SOLO ALUMNO Y LO MUESTRA    
+@app.route('/alumnos/<matricula>', methods=['GET'])
+def leer_alumno(matricula):
     try:
         cursor = conexion.connection.cursor()
-        sql = "SELECT codigo, nombre, creditos FROM curso WHERE codigo = '{0}'".format(codigo)
+        sql = "SELECT nombre, apellido1, matricula FROM alumnos WHERE matricula = '{0}'".format(matricula)
         cursor.execute(sql)
         datos = cursor.fetchone()
         if datos != None:
-            curso = {'codigo':datos[0], 'nombre':datos[1], 'creditos': datos[2]}
-            return jsonify({'curso':curso, 'mensaje':"Curso encontrado!"})
+            alumno = {'nombre':datos[0], 'apellido1':datos[1], 'matricula': datos[2]}
+            return jsonify({'alumno':alumno, 'mensaje':"alumno encontrado!"})
     except Exception as ex:
-        return jsonify({'mensaje':"Curso no encontrado!"})  
-    
-#REGISTRA UN NUEVO CURSO
-@app.route('/cursos', methods=['POST'])
-def registrar_curso():
-    #print(request.json)
-    try:
-        cursor = conexion.connection.cursor()
-
-        #TAREA COMPROBAR SI EL CURSO YA EXISTE PARA EVITAR ERRORES EN LA PETICION
-
-        sql="""INSERT INTO curso (codigo, nombre, creditos) 
-        VALUES ('{0}','{1}','{2}')""".format(request.json['codigo'], #INSERTA LOS VALORES DIRECTAMENTE DEL JSON
-        request.json['nombre'], request.json['creditos'])
-        cursor.execute(sql)
-        conexion.connection.commit() #confirma la acción de inserción
-        return jsonify({'mensaje':"Curso registrado!"})
-    except Exception as ex:
-        return jsonify({'mensaje':"Error"})
-
-@app.route('/cursos/<codigo>', methods=['PUT'])
-def actualizar_curso(codigo):
-    try:
-        cursor = conexion.connection.cursor()
-
-        sql="""UPDATE curso SET nombre = '{0}', creditos = '{1}'
-        WHERE  codigo = '{2}'""".format(request.json['nombre'], request.json['creditos'], codigo)
-        cursor.execute(sql)
-        conexion.connection.commit() #confirma la acción de inserción
-        return jsonify({'mensaje':"Curso actualizado!"})
-    except Exception as ex:
-        return jsonify({'mensaje':"Error"})
-    
-#ELIMINA UN CURSO, SIMILAR A AGREGAR
-@app.route('/cursos/<codigo>', methods=['DELETE'])
-def eliminar_curso(codigo):
-    try:
-        cursor = conexion.connection.cursor()
-
-        #TAREA COMPROBAR SI EL CURSO NO EXISTE PARA EVITAR EJECUTAR LA PETICION
-
-        sql="DELETE FROM curso WHERE codigo = '{0}'".format(codigo)
-        cursor.execute(sql)
-        conexion.connection.commit() #confirma la acción de inserción
-        return jsonify({'mensaje':"Curso eliminado"})
-    except Exception as ex:
-        return jsonify({'mensaje':"Error"})
-
- 
+        return jsonify({'mensaje':"alumno no encontrado!"})  
 
 #EN CASO QUE EL USUARIO ACCEDA A UNA RUTA NO ENCONTRADA
 def pagina_no_encontrada(error):
