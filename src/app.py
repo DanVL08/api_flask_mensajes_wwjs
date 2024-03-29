96be772648bf5176a0a44caea3f9d7314f82f174
Daniel Vargas <daniellopez882017@gmail.com>
2024-03-29 17:28:04 -0600

    se modificaron las funciones alumnos_con_pago_mes_actualy listar_alumnos para solicitar mas datos de la base de datos

--- src/app.py
+++ src/app.py
@@ -10,15 +10,15 @@ conexion = MySQL(app)
 def listar_alumnos():
     try:
         cursor = conexion.connection.cursor()
-        sql = "SELECT nombre, apellido1, matricula FROM alumnos"
+        sql = "SELECT nombre, apellido1, matricula, telefono FROM alumnos"
         cursor.execute(sql)
         datos = cursor.fetchall()
         alumnos = []
         #AGREGAR LOS DATOS DE LOS ALUMNOS A UN JSON
         for fila in datos:
-            alumno = {'nombre':fila[0], 'apellido1':fila[1], 'matricula': fila[2]}
+            alumno = {'nombre':fila[0], 'apellido1':fila[1], 'matricula': fila[2],'telefono': fila[3]}
             alumnos.append(alumno)
-        return jsonify({'alumnos':alumnos, 'mensaje':"Alumnos listados!"})
+        return jsonify({'alumnos':alumnos})
     except Exception as ex:
         return jsonify({'mensaje':"Error"})
 
@@ -36,44 +36,8 @@ def leer_alumno(matricula):
     except Exception as ex:
         return jsonify({'mensaje':"alumno no encontrado!"})  
 
-@app.route('/pagos/<matricula>', methods=['GET'])
-def consultar_pagos(matricula):
-    try:
-        cursor = conexion.connection.cursor()
-        
-        # Consultar el ID del alumno, nombre y apellido a partir de la matrícula
-        sql_id_alumno = "SELECT alumno_id, nombre, apellido1 FROM alumnos WHERE matricula = '{0}'".format(matricula)
-        cursor.execute(sql_id_alumno)
-        datos_id_alumno = cursor.fetchone()
-        
-        if datos_id_alumno is not None:
-            alumno_id = datos_id_alumno[0]
-            nombre = datos_id_alumno[1]
-            apellido1 = datos_id_alumno[2]
-            
-            # Consultar los pagos del alumno a partir de su ID
-            sql_pagos = "SELECT fecha_pago, monto, estado_pago FROM pagos WHERE alumno_id = '{0}'".format(alumno_id)
-            cursor.execute(sql_pagos)
-            datos_pagos = cursor.fetchall()
-            
-            # Crear lista de pagos
-            pagos = []
-            for pago in datos_pagos:
-                pagos.append({'fecha_pago': pago[0], 'monto': pago[1], 'estado_pago': pago[2]})
-            
-            # Crear objeto JSON de respuesta
-            respuesta = {'alumno_id': alumno_id, 'nombre': nombre, 'apellido1': apellido1, 'pagos': pagos}
-            return jsonify(respuesta)
-        
-        else:
-            return jsonify({'mensaje': "Alumno no encontrado"})
-    
-    except Exception as ex:
-        return jsonify({'mensaje': "Error al consultar los pagos del alumno"})
-
-
-@app.route('/pagos_mes_actual/<matricula>', methods=['GET'])
-def pagos_mes_actual(matricula):
+@app.route('/alumnos_con_pago_mes_actual', methods=['GET'])
+def alumnos_con_pago_mes_actual():
     try:
         cursor = conexion.connection.cursor()
         
@@ -84,26 +48,26 @@ def pagos_mes_actual(matricula):
         ultimo_dia_mes_actual = primer_dia_mes_actual.replace(day=28) + datetime.timedelta(days=4)
         ultimo_dia_mes_actual = ultimo_dia_mes_actual - datetime.timedelta(days=ultimo_dia_mes_actual.day)
         
-        # Consultar los pagos del alumno para el mes actual
+        # Consultar la matrícula de los alumnos que han realizado un pago este mes
         sql = """
-            SELECT fecha_pago, monto, estado_pago
-            FROM pagos
-            WHERE alumno_id = (SELECT alumno_id FROM alumnos WHERE matricula = '{0}')
-            AND fecha_pago >= '{1}' AND fecha_pago <= '{2}'
-        """.format(matricula, primer_dia_mes_actual, ultimo_dia_mes_actual)
+            SELECT DISTINCT alumnos.matricula
+            FROM alumnos
+            INNER JOIN pagos ON alumnos.alumno_id = pagos.alumno_id
+            WHERE pagos.fecha_pago >= '{0}' AND pagos.fecha_pago <= '{1}'
+        """.format(primer_dia_mes_actual, ultimo_dia_mes_actual)
         
         cursor.execute(sql)
-        datos_pagos = cursor.fetchall()
+        datos_alumnos = cursor.fetchall()
         
-        # Crear lista de pagos del alumno
-        pagos = [{'fecha_pago': pago[0], 'monto': pago[1], 'estado_pago': pago[2]} for pago in datos_pagos]
+        # Crear lista de matrículas de alumnos con pago este mes
+        matriculas_con_pago = [alumno[0] for alumno in datos_alumnos]
         
         # Crear objeto JSON de respuesta
-        respuesta = {'pagos_mes_actual': pagos, 'mensaje': f"Pagos del alumno con matrícula {matricula} para el mes actual"}
+        respuesta = {'matriculas_con_pago_mes_actual': matriculas_con_pago, 'mensaje': "Matrículas de alumnos con pago este mes"}
         return jsonify(respuesta)
 
     except Exception as ex:
-        return jsonify({'mensaje': "Error al consultar los pagos"})
+        return jsonify({'mensaje': "Error al consultar las matrículas de alumnos con pago este mes"})
 
 #EN CASO QUE EL USUARIO ACCEDA A UNA RUTA NO ENCONTRADA
 def pagina_no_encontrada(error):
