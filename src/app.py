2795f20c33ef9015cf793a1c30e6d6c005a9684d
Daniel Vargas <daniellopez882017@gmail.com>
2024-05-14 18:18:12 -0600

    Se han combinado los endpoints para consultar informacion y vistas de usaurio en app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, url_for, flash, render_template
 from flask_mysqldb import MySQL
 from models import db, ma, Alumnos, Pagos, alumnoSchema, pagoSchema
 from config import config
@@ -6,6 +6,8 @@ import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/prefeco_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#settings
+app.secret_key = 'mysecretkey'
 conexion = MySQL(app)
 db.init_app(app)
 ma.init_app(app)
@@ -14,7 +16,190 @@ alumno_schema =  alumnoSchema()
 alumnos_schema =  alumnoSchema(many=True)
 
 pago_schema =  pagoSchema()
-pagos_schema =  pagoSchema(many=True)  
+pagos_schema =  pagoSchema(many=True)
+#RUTAS DE VISTA DE USUARIO
+#RUTA INICIO, aparecerá la pantalla para mostrar o registrar alumnos
+@app.route('/')
+def index():
+    #SE OBTIENEN LOS DATOS DE LA BD Y SE RETORNAN PLASMADOS EN index.html
+    try:
+        all_alumnos = Alumnos.query.all()
+        resultados = alumnos_schema.dump(all_alumnos)
+        return render_template('index.html', alumnos = resultados)
+    except Exception as e:
+        flash(e)
+        return redirect(url_for('index'))
+
+#RUTA PARA AÑADIR ALUMNOS NUEVOS
+@app.route('/add_alumno', methods=['POST'])
+def add_alumno():
+    #SOLICITAR LOS DATOS DEL FORMULARIO DE index.html
+    if request.method == 'POST':
+        try:
+            #COMPRUEBA SI YA EXISTE UN ALUMNO EN LA BD
+            alumno = Alumnos.query.filter_by(matricula = request.form['matricula']).first()
+            if not alumno:
+                # Crear una instancia de la clase Alumnos con atributos asignados
+                alumno = Alumnos(
+                    nombre          = request.form['nombres'],
+                    apellido1       = request.form['apellido1'],
+                    apellido2       = request.form['apellido2'],
+                    fecha_nacimiento= request.form['fecha_nacimiento'],
+                    grado           = request.form['grado'],
+                    grupo           = request.form['grupo'],
+                    matricula       = request.form['matricula'],
+                    direccion       = request.form['direccion'],
+                    telefono        = request.form['telefono']
+                )
+                
+                db.session.add(alumno)
+                db.session.commit()
+
+                flash('Contacto agregado satisfactoriamente')
+                return redirect(url_for('index'))
+            else:
+                flash(f"La matricula {alumno.matricula} ya está registrada en la base de datos")
+                return redirect(url_for('index'))
+        except Exception as e:
+            flash(e)
+            return redirect(url_for('index'))
+        
+#EJECUTA LA CONSULTA SQL PARA OBTENER UN ALUMNO PARA EDITAR
+@app.route('/edit-alumno/<id>')
+def get_alumno(id):
+        try:
+            alumno_bd = Alumnos.query.filter_by(alumno_id = id).first()
+            return render_template('editar-alumno.html',alumno = alumno_bd)
+        except Exception as e:
+            flash(e)
+
+#SE ENCARGA DE ENVIAR LOS DATOS ACTUALIZADOS DEL FORMULARIO A LA BD
+@app.route('/update-alumno/<id>', methods = ['POST'])
+def update_alumno(id):
+    #SOLICITAR LOS DATOS DEL FORMULARIO DE editar-alumno.html
+    if request.method == 'POST':
+        try:
+            alumno = Alumnos.query.get(id) 
+            alumno.nombre = request.form['nombres']
+            alumno.apellido1 = request.form['apellido1']
+            alumno.apellido2 = request.form['apellido2']
+            alumno.fecha_nacimiento = request.form['fecha_nacimiento']
+            alumno.grado = request.form['grado']
+            alumno.grupo = request.form['grupo']
+            alumno.matricula = request.form['matricula']
+            alumno.direccion = request.form['direccion']
+            alumno.telefono = request.form['telefono']
+            
+            db.session.commit()
+            flash('Alumno actualizado satisfactoriamente.')
+            return(redirect(url_for('index')))
+        except Exception as e:
+            flash(e)
+            return redirect(url_for('index'))
+            
+#ELIMINA UN ALUMNO A APARTIR DE SI ID
+@app.route('/delete-alumno/<id>')
+def delete_alumno(id):
+    try:
+        alumno = Alumnos.query.filter_by(alumno_id = id).first()
+        db.session.delete(alumno)
+        db.session.commit()
+        flash('Alumno eliminado exitosamente')
+        return redirect(url_for('index'))
+    except Exception as e:
+        flash(e)
+        return redirect(url_for('index_pagos'))
+
+#RUTA INICIO, aparecerá la pantalla para mostrar o registrar pagos
+@app.route('/pagos')
+def index_pagos():
+    #SE OBTIENEN LOS DATOS DE LA BD Y SE RETORNAN PLASMADOS EN pagos.html
+    try:
+        all_pagos = Pagos.query.all()
+        resultados = pagos_schema.dump(all_pagos)
+        return render_template('pagos.html', pagos = resultados)
+    except Exception as e:
+        flash(e)
+        return redirect(url_for('index'))
+
+#RUTA PARA AÑADIR ALUMNOS NUEVOS
+@app.route('/add_pago', methods=['POST'])
+def add_pago():
+    #SOLICITAR LOS DATOS DEL FORMULARIO DE index.html
+    if request.method == 'POST':
+        try:
+            alumno = Alumnos.query.filter_by(matricula = request.form['matricula']).first()
+            if alumno:
+                # Crear una instancia de la clase Alumnos con atributos asignados
+                pago = Pagos(
+                    matricula     = request.form['matricula'],
+                    alumno_id     = alumno.alumno_id,
+                    fecha_pago    = request.form['fecha_pago'],
+                    monto         = request.form['monto'],
+                    metodo_pago   = request.form['metodo_pago'],
+                    estado_pago   = request.form['estado_pago'],
+                    concepto_pago = request.form['concepto_pago'],
+                )
+                
+                db.session.add(pago)
+                db.session.commit()
+
+                flash('Pago agregado satisfactoriamente')
+                return redirect(url_for('index_pagos'))
+            else:
+                flash('Matricula no encontrada!!!!!')
+                return redirect(url_for('index_pagos'))
+        except Exception as e:
+            flash(e)
+            return redirect(url_for('index_pagos'))
+
+#EJECUTA LA CONSULTA SQL PARA OBTENER UN ALUMNO PARA EDITAR
+@app.route('/editar-pago/<id>')
+def get_pago(id):
+        try:
+            print(id)
+            pago_bd = Pagos.query.filter_by(pago_id = id).first()
+            return render_template('editar-pago.html', pago = pago_bd)
+        except Exception as e:
+            flash(e)
+
+#SE ENCARGA DE ENVIAR LOS DATOS ACTUALIZADOS DEL FORMULARIO A LA BD
+@app.route('/update-pago/<id>', methods = ['POST'])
+def update_pago(id):
+    #SOLICITAR LOS DATOS DEL FORMULARIO DE editar-alumno.html
+    if request.method == 'POST':
+        try:
+            pago = Pagos.query.get(id)
+            print(pago.alumno_id)
+            pago.matricula = request.form['matricula']
+            pago.fecha_pago = request.form['fecha_pago']
+            pago.monto = request.form['monto']
+            pago.metodo_pago = request.form['metodo_pago']
+            pago.estado_pago = request.form['estado_pago']
+            pago.concepto_pago = request.form['concepto_pago']
+            
+            db.session.commit()
+            flash('Pago actualizado satisfactoriamente.')
+            return(redirect(url_for('index_pagos')))
+        except Exception as e:
+            flash(e)
+            return redirect(url_for('index_pagos')) 
+
+#ELIMINA UN ALUMNO A APARTIR DE SI ID
+@app.route('/borrar-pago/<id>')
+def delete_pago(id):
+    try:
+        pago = Pagos.query.filter_by(pago_id = id).first()
+        db.session.delete(pago)
+        db.session.commit()
+        flash('Pago eliminado exitosamente')
+        return redirect(url_for('index_pagos'))
+    except Exception as e:
+        flash(e)
+        return redirect(url_for('index_pagos'))
+#######################################
+#ENDOPOINTS PARA CONSULTAR INFORMACIÓN#
+#######################################
 
 @app.route('/alumnos', methods=['GET'])
 def get_alumnos_con_alchemy():
@@ -71,6 +256,8 @@ def matriculas_pago_mes_actual():
     matriculas_list = [matricula[0] for matricula in matriculas]
 
     return jsonify({"matriculas_con_pago_mes_actual": matriculas_list,'mensaje': "Matrículas de alumnos con pago este mes"})
+
+            
 
 #EN CASO QUE EL USUARIO ACCEDA A UNA RUTA NO ENCONTRADA
 def pagina_no_encontrada(error):
